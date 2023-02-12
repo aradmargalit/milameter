5078062771e8b3533a246d2ea8fbe173128ecdc1
Arad Margalit <aradmar@gmail.com>
2023-02-11 18:35:53 -0800

    extract map to component

--- src/pages/p/activity/[stravaActivityId].tsx
+++ src/pages/p/activity/[stravaActivityId].tsx
@@ -8,6 +8,7 @@ import mapboxgl from 'mapbox-gl';
 import { useEffect, useRef } from 'react';
 import { swapLatLong } from '@/utils/coordinateUtils';
 import { expandBounds, makeLineFromCoordinates } from '@/utils/mapboxUtils';
+import { DetailedActivityMap } from '@/components/ActivityMap/DetailedActivityMap';
 
 type Data = {
   activity: Activity | null;
@@ -49,62 +50,13 @@ export const getServerSideProps: GetServerSideProps<{ data: Data }> = async (
 export default function StravaActivityDetailPage({
   data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  // This is fine for now, eventually we'll lock down a production token to just our domain
-  // https://docs.mapbox.com/accounts/guides/tokens/#url-restrictions
-  mapboxgl.accessToken =
-    'pk.eyJ1IjoiYXJhZG1hcmdhbGl0IiwiYSI6ImNsZTBkbnk1NDE5eGkzbnIzNGE3Zmx1NnQifQ.qOh6-x2DMYLm9qcp9ck-MA';
-
-  const mapContainer = useRef<HTMLElement | null>(null);
-  const map = useRef<mapboxgl.Map | null>(null);
-
-  useEffect(() => {
-    if (map.current || !mapContainer.current) return; // initialize map only once
-
-    // if there are no coordinates, there's nothing to show here
-    if (!data.activity) {
-      return;
-    }
-
-    const coordinates = polyline.decode(data.activity.map.polyline);
-    const correctedCoordinates = swapLatLong(coordinates);
-    const bounds = expandBounds(correctedCoordinates);
-    const geoJSON = makeLineFromCoordinates(correctedCoordinates);
-
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/outdoors-v12',
-      attributionControl: false,
-      bounds,
-    });
-
-    map.current.on('load', function () {
-      if (map.current === null) return;
-
-      map.current.addSource('route', { type: 'geojson', data: geoJSON });
-      map.current.addLayer({
-        id: 'route',
-        type: 'line',
-        source: 'route',
-        paint: {
-          'line-color': 'black',
-          'line-opacity': 0.75,
-          'line-width': 10,
-        },
-      });
-
-      map.current.fitBounds(bounds, {
-        padding: 80, // add some spacing around the coordinates
-      });
-    });
-  });
-
   if (!data.activity) {
     return <p>Could not find activity</p>;
   }
 
   return (
     <div>
-      <Box ref={mapContainer} sx={{ height: '700px' }} />
+      <DetailedActivityMap activity={data.activity} />
     </div>
   );
 }
