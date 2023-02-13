6126dcb6e8344d2dd3e20b528aa15db5e528a3c0
Arad Margalit <aradmar@gmail.com>
2023-02-12 21:15:26 -0800

    stricter types

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth, { AuthOptions, TokenSet } from 'next-auth';
 import { JWT } from 'next-auth/jwt';
 import StravaProvider from 'next-auth/providers/strava';
 
-async function refreshAccessToken(token: JWT) {
+async function refreshAccessToken(token: JWT): Promise<JWT> {
   try {
     // https://developers.strava.com/docs/authentication/#refreshingexpiredaccesstokens
     const response = await fetch('https://www.strava.com/api/v3/oauth/token', {
@@ -22,8 +22,8 @@ async function refreshAccessToken(token: JWT) {
 
     return {
       ...token, // Keep the previous token properties
-      accessToken: tokens.access_token,
-      expiresAt: tokens.expires_at,
+      accessToken: tokens.access_token!,
+      expiresAt: tokens.expires_at!,
       // Fall back to old refresh token, but note that
       // many providers may only allow using a refresh token once.
       refreshToken: tokens.refresh_token ?? token.refreshToken,
@@ -48,13 +48,13 @@ export const authOptions: AuthOptions = {
     async jwt({ token, account }) {
       // Persist the OAuth access_token and or the user id to the token right after signin
       if (account) {
-        token.accessToken = account.access_token;
-        token.refreshToken = account.refresh_token;
-        token.expiresAt = account.expires_at; // UNIX epoch
+        token.accessToken = account.access_token!;
+        token.refreshToken = account.refresh_token!;
+        token.expiresAt = account.expires_at!; // UNIX epoch
         return token;
       }
 
-      if (!token.expiresAt || Date.now() < token.expiresAt) {
+      if (Date.now() < token.expiresAt) {
         // If the access token has not expired yet, return it
         return token;
       } else {
@@ -69,7 +69,7 @@ export const authOptions: AuthOptions = {
   },
 
   pages: {
-    signIn: '/auth/signin',
+    signIn: '/',
   },
 };
 
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,9 +4,9 @@ declare module 'next-auth/jwt' {
   /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
   interface JWT extends DefaultJWT {
     /** OpenID ID Token */
-    accessToken?: string;
-    refreshToken?: string;
-    expiresAt?: number;
+    accessToken: string;
+    refreshToken: string;
+    expiresAt: number;
     error?: "RefreshAccessTokenError"
   }
 }
