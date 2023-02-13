b6416c44b78a8d1469ddcc501afbafb68c5c2cfa
Arad Margalit <aradmargalit@users.noreply.github.com>
2023-02-13 06:57:57 -0800

    Refresh tokens (#16)
    * refresh token working, maybe
    
    * stricter types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,18 @@ import StravaLoginButtonContainer from '@/components/StravaLoginButton';
 import { Sheet } from '@mui/joy';
 import { GetServerSideProps } from 'next';
 import { getServerSession } from 'next-auth';
+import { signIn } from 'next-auth/react';
 import Head from 'next/head';
 import { authOptions } from './api/auth/[...nextauth]';
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   // If we have an access token, redirect to the activities page
   const session = await getServerSession(req, res, authOptions);
+
+  // If the session has expired, try to refresh it
+  if (session?.error === 'RefreshAccessTokenError') {
+    signIn(); // Force sign in to hopefully resolve error
+  }
 
   if (session) {
     return {
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,6 +4,15 @@ declare module 'next-auth/jwt' {
   /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
   interface JWT extends DefaultJWT {
     /** OpenID ID Token */
-    accessToken?: string;
+    accessToken: string;
+    refreshToken: string;
+    expiresAt: number;
+    error?: "RefreshAccessTokenError"
+  }
+}
+
+declare module "next-auth" {
+  interface Session {
+    error?: "RefreshAccessTokenError"
   }
 }
