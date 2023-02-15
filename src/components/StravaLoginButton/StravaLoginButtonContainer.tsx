09ffb6d04b4eceac959bd16d254743ff070674f7
Arad Margalit <aradmargalit@users.noreply.github.com>
2023-02-14 22:03:07 -0800

    fun landing text (#26)

--- src/components/StravaLoginButton/StravaLoginButtonContainer.tsx
+++ src/components/StravaLoginButton/StravaLoginButtonContainer.tsx
@@ -1,13 +1,20 @@
-import { StravaLoginButton } from '@/components/StravaLoginButton/StravaLoginButton';
+import {
+  StravaLoginButton,
+  StravaLoginButtonProps,
+} from '@/components/StravaLoginButton/StravaLoginButton';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { signIn } from 'next-auth/react';
 
-export function StravaLoginButtonContainer() {
+type StravaLoginButtonContainerProps = Omit<StravaLoginButtonProps, 'onClick'>;
+
+export function StravaLoginButtonContainer(
+  props: StravaLoginButtonContainerProps
+) {
   // We only support one provider, if that changes, update this
   const { id, name } = authOptions.providers[0];
   if (name !== 'Strava') {
     throw new Error('Strava should be the only provider configured.');
   }
 
-  return <StravaLoginButton onClick={() => signIn(id)} />;
+  return <StravaLoginButton onClick={() => signIn(id)} {...props} />;
 }
