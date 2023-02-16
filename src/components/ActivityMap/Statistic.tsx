33bfeaf7989e2904d5dbe69969de87b9649f2393
Eshed Margalit <eshed.margalit@gmail.com>
2023-02-15 19:04:01 -0800

    more formatting

--- src/components/ActivityMap/Statistic.tsx
+++ src/components/ActivityMap/Statistic.tsx
@@ -1,4 +1,4 @@
-import { Card, Stack, Typography } from '@mui/joy';
+import { Card, CardOverflow, Divider, Stack, Typography } from '@mui/joy';
 
 type StatisticProps = {
   name: string;
@@ -7,16 +7,19 @@ type StatisticProps = {
 };
 export function Statistic({ name, value, units }: StatisticProps) {
   return (
-    <Card variant="outlined" sx={{ width: 120, height: 120, padding: 1 }}>
-      <Typography level="h2" sx={{ fontSize: 'md', mt: 0, mb: 1 }}>
-        {name}
-      </Typography>
-      <Stack direction="row" sx={{ mt: 0.5, mb: 1 }}>
-        <Typography level="body1" sx={{ mr: 0.5 }}>
-          {value.toFixed(2)}
+    <Card variant="outlined" sx={{ width: 240, height: 80 }}>
+      <CardOverflow>
+        <Typography level="h2" sx={{ fontSize: 'md', mt: 1, mb: 1 }}>
+          {name}
         </Typography>
-        <Typography level="body3">{units}</Typography>
-      </Stack>
+        <Divider />
+        <Stack direction="row" sx={{ mt: 0.5, mb: 1 }}>
+          <Typography level="body1" sx={{ mr: 0.5 }}>
+            {value.toFixed(2)}
+          </Typography>
+          <Typography level="body3">{units}</Typography>
+        </Stack>
+      </CardOverflow>
     </Card>
   );
 }
