40df762ccd15f570fb2d339a4462fe29b410fde0
Eshed Margalit <eshed.margalit@gmail.com>
2023-02-18 14:22:14 -0800

    PR feedback

--- src/components/ActivityMap/LiveSeparation.tsx
+++ src/components/ActivityMap/LiveSeparation.tsx
@@ -1,17 +1,16 @@
-import { Box } from '@mui/joy';
+import { Typography } from '@mui/joy';
 
 const TOGETHERNESS_THRESH = 35; // meters
 
-type LiveSeparationProps = { separation: number | null };
+type LiveSeparationProps = { separation: number };
 
 export function LiveSeparation({ separation }: LiveSeparationProps) {
-  const runningTogether =
-    separation === null || separation < TOGETHERNESS_THRESH;
+  const runningTogether = separation < TOGETHERNESS_THRESH;
   const inner = runningTogether
     ? 'Running Together'
     : `${separation.toFixed(0)}m Apart`;
   return (
-    <Box
+    <Typography
       sx={{
         display: 'flex',
         mt: 2,
@@ -20,6 +19,6 @@ export function LiveSeparation({ separation }: LiveSeparationProps) {
       }}
     >
       {inner}
-    </Box>
+    </Typography>
   );
 }
