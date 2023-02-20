cf887d5b2bf26143b5f03596f10ba66e6fa4fb6d
Eshed Margalit <eshed.margalit@gmail.com>
2023-02-20 15:55:09 -0800

    implement le suggestion de arbo

--- src/utils/distanceUtils.test.ts
+++ src/utils/distanceUtils.test.ts
@@ -1,46 +1,23 @@
 import { computePace, METERS_PER_MILE } from './distanceUtils';
-test('4 miles in 36 minutes is 9:00', () => {
-  const fourMilesInMeters = 4 * METERS_PER_MILE;
-  expect(
-    computePace({
-      distance: fourMilesInMeters,
-      elapsedTime: 36 * 60 + 1,
-      coordinates: [],
-      records: [],
-    })
-  ).toBe('09:00');
-});
 
-test('1 mile in 6 minutes is 6:00', () => {
-  const oneMileInMeters = 1 * METERS_PER_MILE;
-  expect(
-    computePace({
-      distance: oneMileInMeters,
-      elapsedTime: 6 * 60 + 1,
-      coordinates: [],
-      records: [],
-    })
-  ).toBe('06:00');
-});
-
-test('2 miles in 41 minutes is 20:30', () => {
-  expect(
-    computePace({
-      distance: 2 * METERS_PER_MILE,
-      elapsedTime: 41 * 60 + 1,
-      coordinates: [],
-      records: [],
-    })
-  ).toBe('20:30');
-});
-
-test('2 miles in 400 minutes is 200:00', () => {
-  expect(
-    computePace({
-      distance: 2 * METERS_PER_MILE,
-      elapsedTime: 400 * 60 + 1,
-      coordinates: [],
-      records: [],
-    })
-  ).toBe('200:00');
+describe('computePace', () => {
+  it.each<[number, number, string]>([
+    [4, 36, '09:00'],
+    [1, 6, '06:00'],
+    [2, 41, '20:30'],
+    [2, 400, '200:00'],
+  ])(
+    'outputs correct pace with %s miles and %s minutes, output is: %s',
+    (miles, minutes, expectedPace) => {
+      const meters = miles * METERS_PER_MILE;
+      expect(
+        computePace({
+          distance: meters,
+          elapsedTime: minutes * 60 + 1,
+          coordinates: [],
+          records: [],
+        })
+      ).toBe(expectedPace);
+    }
+  );
 });
