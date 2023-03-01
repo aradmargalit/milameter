76f2c3a2e7f14fff76216de98f7f258e1f62f931
Arad Margalit <aradmargalit@users.noreply.github.com>
2023-02-28 20:44:05 -0800

    Fix Login button flicker (#49)
    * fix flashing login button
    
    * dont need mount check

--- src/components/StravaLoginButton/StravaLoginButtonContainer.tsx
+++ src/components/StravaLoginButton/StravaLoginButtonContainer.tsx
@@ -1,4 +1,5 @@
-import { signIn } from 'next-auth/react';
+import { Button } from '@mui/joy';
+import { signIn, signOut } from 'next-auth/react';
 
 import {
   StravaLoginButton,
@@ -7,14 +8,37 @@ import {
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
 type StravaLoginButtonContainerProps = Omit<StravaLoginButtonProps, 'onClick'>;
+import { LogoutOutlined } from '@mui/icons-material';
+import { useSession } from 'next-auth/react';
 
 export function StravaLoginButtonContainer(
   props: StravaLoginButtonContainerProps
 ) {
+  const { status } = useSession();
+
+  const isLoggedIn = status === 'authenticated';
+
   // We only support one provider, if that changes, update this
   const { id, name } = authOptions.providers[0];
   if (name !== 'Strava') {
     throw new Error('Strava should be the only provider configured.');
+  }
+
+  if (status === 'loading') {
+    return null;
+  }
+
+  if (isLoggedIn) {
+    return (
+      <Button
+        startDecorator={<LogoutOutlined />}
+        variant="soft"
+        onClick={() => signOut()}
+        color="danger"
+      >
+        Log Out
+      </Button>
+    );
   }
 
   return <StravaLoginButton onClick={() => signIn(id)} {...props} />;
