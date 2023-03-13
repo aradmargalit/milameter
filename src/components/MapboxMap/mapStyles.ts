9440ef0e3422cbf99f34e1fd7e4ad4c8d2d7dc07
Arad Margalit <aradmar@gmail.com>
2023-03-12 17:15:49 -0700

    clean up

--- src/components/MapboxMap/mapStyles.ts
+++ src/components/MapboxMap/mapStyles.ts
@@ -1,17 +1,3 @@
-// export const mapStyles = {
-//   streets: ['Streets', 'mapbox://styles/mapbox/streets-v12'],
-//   outdoors: ['Outdoors', 'mapbox://styles/mapbox/outdoors-v12'],
-//   light: ['Light', 'mapbox://styles/mapbox/light-v11'],
-//   dark: ['Dark', 'mapbox://styles/mapbox/dark-v11'],
-//   satellite: ['Satellite', 'mapbox://styles/mapbox/satellite-v9'],
-//   satelliteStreet: [
-//     'Satellite Street',
-//     'mapbox://styles/mapbox/satellite-streets-v12',
-//   ],
-//   day: ['Day', 'mapbox://styles/mapbox/navigation-day-v1'],
-//   night: ['Night', 'mapbox://styles/mapbox/navigation-night-v1'],
-// } as const;
-
 export const mapStyles = [
   { label: 'Streets', value: 'mapbox://styles/mapbox/streets-v12' },
   { label: 'Outdoors', value: 'mapbox://styles/mapbox/outdoors-v12' },
@@ -27,6 +13,3 @@ export const mapStyles = [
 ] as const;
 
 export type MapStyle = (typeof mapStyles)[number]['label'];
-// export type MapStyleOption = (typeof mapStyles)[MapStyleKey];
-// export type MapStyleName = MapStyleOption[number][0];
-// export type MapStyleURL = MapStyleOption[number][1];
