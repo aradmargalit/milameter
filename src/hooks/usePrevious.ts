3283c4623db2ad1dfae5295aad0f82cdbf588880
Arad Margalit <aradmar@gmail.com>
2025-04-26 10:52:31 -0700

    prettier-ify

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -2,10 +2,9 @@ import { useEffect, useRef } from 'react';
 
 // https://usehooks.com/usePrevious/
 export function usePrevious<T>(value: T) {
-  const ref = useRef(undefined);
+  const ref = useRef<T>(undefined);
   // Store current value in ref
   useEffect(() => {
-    // @ts-ignore
     ref.current = value;
   }, [value]); // Only re-run if value changes
 
